e6b7094ed2cf1e3e772149ac61e1c065af13f313
Justin <planovskyjus@sehs.net>
2016-12-05 16:47:02 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//Use these for onmouseover and onmouseout 
+function oneS(){
+   document.getElementById("cover").style.width = "0.5%";
+}
+function oneG(){
+   document.getElementById("cover").style.width = "100%";
+}
+//Use this for another row
+// function twoS(){
+//    document.getElementById("cover-two").style.width = "0.5%";
+// }
+// function twoG(){
+//    document.getElementById("cover-two").style.width = "100%";
+// }
+// function threeS(){
+//    document.getElementById("cover-three").style.width = "0.5%";
+// }
+// function threeG(){
+//    document.getElementById("cover-three").style.width = "100%";
+// }
